e917b3efa0a791de34029dbc2846e9b0be725771
Brian Henry <brianhenrytech@gmail.com>
2025-03-18 10:48:08 -0500

    Fixed render Issue

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,9 +1,19 @@
 import { useEffect } from "react";
 
-export const RockList = ({ rocks, fetchRocks }) => {
+export const RockList = ({ loc, rocks, fetchRocks }) => {
   useEffect(() => {
-    fetchRocks();
-  }, []);
+    fetchRocks(loc);
+  }, [loc.to]);
+
+  const deleteRock = async (rockId) => {
+    const response = await fetch(`http://localhost:8000/rocks/${rockId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`,
+      },
+    });
+    fetchRocks(loc);
+  };
 
   const displayRocks = () => {
     if (rocks && rocks.length) {
@@ -12,8 +22,21 @@ export const RockList = ({ rocks, fetchRocks }) => {
           key={`key-${rock.id}`}
           className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50"
         >
-          {rock.name} ({rock.type.label}) weighs {rock.weight} kg in the
-          colloection of {rock.user?.first_name} {rock.user?.last_name}
+          {rock.name} ({rock.type.label}) weighs {rock.weight} kg
+          {loc.to === "allrocks" ? (
+            `in the colloection of ${rock.user?.first_name} ${rock.user?.last_name}`
+          ) : (
+            <>
+              <div className="float-right">
+                <button
+                  className="border-2 border-violet-900 rounded-md p-1"
+                  onClick={() => deleteRock(rock.id)}
+                >
+                  delete
+                </button>
+              </div>
+            </>
+          )}
         </div>
       ));
     }
@@ -23,7 +46,10 @@ export const RockList = ({ rocks, fetchRocks }) => {
 
   return (
     <>
-      <h1 className="text-3xl">Rock List</h1>
+      <h1 className="text-3xl">
+        {" "}
+        {loc.to === "allrocks" ? "Rock List" : "My rocks"}
+      </h1>
       {displayRocks()}
     </>
   );
